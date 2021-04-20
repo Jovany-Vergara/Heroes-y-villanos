723255dc667918f96953f9e206e1a2395c3bcefe
unknown <78619347+Jovany-Vergara@users.noreply.github.com>
2021-04-20 16:17:06 -0500

    se hizo un modelo de heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { ListadoComponent } from './heroes/heroe/listado/listado.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroeComponent } from './heroes/heroe/heroe.component';
+import { ContadorComponent } from './contador/contador.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroeComponent,
-    ListadoComponent
-  ],
+    ListadoComponent,
+      ContadorComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
