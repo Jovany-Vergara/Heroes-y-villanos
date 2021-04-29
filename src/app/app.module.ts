d9c497dbdb82874ac7d2392d50a239c9976b4017
unknown <78619347+Jovany-Vergara@users.noreply.github.com>
2021-04-29 18:48:27 -0500

    se le agrego el formsng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,24 @@ import { AppComponent } from './app.component';
 import { HeroeComponent } from './heroes/heroe/heroe.component';
 import { ContadorComponent } from './contador/contador.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { VillanosComponent } from './villanos/villanos.component';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroeComponent,
     ListadoComponent,
-      ContadorComponent
+      ContadorComponent,
+      VillanosComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
