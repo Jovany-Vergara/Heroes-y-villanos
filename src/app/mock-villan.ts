2acbf846f770191f148410474bebed54ecdc498d
unknown <78619347+Jovany-Vergara@users.noreply.github.com>
2021-04-29 18:52:04 -0500

    el ts de villanos

---
+++ src/app/mock-villan.ts
@@ -0,0 +1,9 @@
+import {Villano} from './villan';
+export const VILLANOS: Villano[] = [
+    {id: 1, name: 'Joker', nivel: 553, poder: 'Agilidad. Coeficiente intelectual de genio'},
+    {id: 2, name: 'Thanos', nivel: 26003, poder: 'fuerza sobrehumana, resistencia y durabilidad'},
+    {id: 3, name: 'Darkseid', nivel: 300003, poder: 'Inmortalidad Atributos físicos sobrehumanos'},
+    {id: 4, name: 'Brainiac', nivel: 11003, poder: 'varían drásticamente'},
+    {id: 5, name: 'Goku Black', nivel: 22003, poder: 'ganar un impulso enorme de poder al instante'},
+    {id: 6, name: 'Siniestro', nivel: 900, poder: ' poderes mentales'}
+];
